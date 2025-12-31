e49cdc0930c933a48ac0ed230acbaad5b33e2c12
surendra <surendrakumarreddy79@gmail.com>
2025-12-31 11:07:32 +0530

    google login completed

---
+++ src/app/lib/passport.js
@@ -0,0 +1,37 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import connectDB from "../lib/db";
+import User from "../lib/models/User";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google/callback`,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        await connectDB();
+
+        const email = profile.emails[0].value;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            email,
+            googleId: profile.id,
+            disclaimerAccepted: false,
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+export default passport;

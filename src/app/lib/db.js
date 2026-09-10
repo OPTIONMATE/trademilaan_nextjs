d8e86ee89a11d683774ef4234e42fd594a071dda
harshavardhanbalaga <harshabalaga45@gmail.com>
2026-09-10 13:40:11 +0530

    seperating prod & dev databases

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) throw new Error("MONGO_URI not defined");
 
+const DB_NAME = process.env.DB_NAME;
+if (!DB_NAME) throw new Error("DB_NAME not defined");
+
 let cached = global.mongoose;
 if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
@@ -11,6 +14,7 @@ export default async function connectDB() {
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGO_URI, {
+      dbName: DB_NAME,
       bufferCommands: false,
     });
   }

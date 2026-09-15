ee68da4bc9764f99a4db8c2041eeeeb1ab69fc55
harshavardhanbalaga <harshabalaga45@gmail.com>
2026-09-15 12:25:26 +0530

    RA Signature issue fixed - testing

--- src/components/RASignature.jsx
+++ src/components/RASignature.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function RASignature() {
   return (
     <img
-      src={require("../WhatsApp Image 2026-03-05 at 11.24.13 PM.jpeg")}
+      src="/ra-signature.jpeg"
       alt="RA Signature"
       style={{ maxWidth: 120, maxHeight: 60, objectFit: "contain" }}
     />
